4aef7acfd5aed0e6c0db8e1a827d1f2a8d0421a1
VadimRogo <mrk.main.03@gmail.com>
2022-12-13 14:22:50 +0300

    Make more coins

--- data.py
+++ data.py
@@ -10,6 +10,9 @@ import requests
 key_client = 'OIOP5aA2mZVQ9om2ZVdV5MdO7UnxXPM4n5DTL0QmVQMmbhNZxb3g9F4NaaoghnyW'
 secret = 'OvsYPIeQfh5Cz4QgzVSKwRZe8HpQOQqjWzZBugmiAqyQxYuIpJSIK6XfKCvhTCYK'
 
+Coins = ["OCEANBUSD", "TVKBUSD"]
+CoinsQty = [153, 595]
+
 
 client = Client(key_client, secret)
 
@@ -39,7 +42,7 @@ def MakingPlots():
     plt.show()
 
 
-def Buy():
+def Buy(Coin, qty):
     global price, flag
     if flag == False:
         try:
@@ -47,21 +50,22 @@ def Buy():
             price = df['Close'][-1]
             qty = 153       
             order = client.create_order(
-                    symbol='OCEANBUSD',
+                    symbol=Coin,
                     side=Client.SIDE_BUY,
                     type=Client.ORDER_TYPE_MARKET,
-                    quantity = 153
+                    quantity = qty
                     )
-            Tiket(price, qty)
+            Tiket(Coin, price, qty)
             flag = True
         except Exception as Ext:
             print(Ext)
 
-def Tiket(price, qty):
+def Tiket(symbol, price, qty):
     global Tikets
     sellpriceprofit = price + price / 100
     sellpriceloss = price - price / 100
     Tik = {
+        'symbol' : symbol,
         'price' : price,
         'sellpriceprofit' : sellpriceprofit,
         'sellpriceloss' : sellpriceloss,
@@ -80,7 +84,7 @@ def Sell(T):
                     symbol='OCEANBUSD',
                     side=Client.SIDE_SELL,
                     type=Client.ORDER_TYPE_MARKET,
-                    quantity = 152
+                    quantity = T['qty'] - T['qty'] / 1000
                     )
             T['sold'] = True
             flag = False
@@ -88,16 +92,20 @@ def Sell(T):
             print(Ext)
 
 for i in range(100000000):
-    df = getminutedata('OCEANBUSD', '1m', '100')
-    df['RSI'] = ta.momentum.rsi(df.Close, window = 14)
-    price = df['Close'][-1]
-    for j in Tikets:
-        if j['sold'] == False and (j['sellpriceprofit'] > price or j['sellpriceloss']):
-            Sell(j)
-    if flag == False and df['RSI'][-1] < 35:
-        Buy()
-    print('Cycle number - ', i, df['Close'][-1])
-    print(df['RSI'][-1])
+    for Coin in Coins:
+        df = getminutedata(Coin, '1m', '100')
+        df['RSI'] = ta.momentum.rsi(df.Close, window = 14)
+        price = df['Close'][-1]
+        for j in Tikets:
+            if j['symbol'] == Coin and j['sold'] == False and (j['sellpriceprofit'] > price or j['sellpriceloss'] > price):
+                Sell(j)
+        if flag == False and df['RSI'][-1] < 35:
+            Buy(Coin, CoinsQty[Coins.index(Coin)])
+        print(Coin, CoinsQty[Coins.index(Coin)])
+        
+        print('Cycle number - ', i, df['Close'][-1])
+        print(df['RSI'][-1])
+    
     time.sleep(60)
 
 
