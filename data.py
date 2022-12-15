c738817c8b4d6cadbffa324cb7dddb97a72b0eca
VadimRogo <mrk.main.03@gmail.com>
2022-12-15 15:16:14 +0300

    Update 15.12 2

--- data.py
+++ data.py
@@ -61,8 +61,8 @@ def Buy(Coin, qty):
 
 def Tiket(symbol, price, qty):
     global Tikets
-    sellpriceprofit = price + price * (0.5 / 100)
-    sellpriceloss = price - price * (0.8 / 100)
+    sellpriceprofit = price + price * 0.5
+    sellpriceloss = price - price * 0.8
     Tik = {
         'time' : datetime.now().strftime("%Y-%m-%d %H:%M"),
         'symbol' : symbol,
