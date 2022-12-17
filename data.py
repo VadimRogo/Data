150a01a0a2cd528d64084e679d017a874688e31a
VadimRogo <mrk.main.03@gmail.com>
2022-12-17 11:15:12 +0300

    17.12 Update 1

--- data.py
+++ data.py
@@ -45,7 +45,7 @@ def Buy(Coin, qty):
         try:
             price = df['Close'][-1]   
             print('Buy - ', price)
-            qty = math.floor(19 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)]            
+            qty = math.floor(10 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)]            
             order = client.create_order(
                     symbol=Coin+'BUSD',
                     side=Client.SIDE_BUY,
@@ -122,11 +122,11 @@ def main():
                     print('Balance in start - {}, Balance in End - {}, percents - {}'.format(balanceStart, balanceEnd, float(balanceStart) / float(balanceEnd)))
             
             if flag == False and df['RSI'][-1] < 35 and df['SMA 30'][-1] > df['SMA 100'][-1]:
-                Buy(Coin, math.floor(19 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)])
+                Buy(Coin, math.floor(10 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)])
             if df['RSI'][-1] < 35 and df['SMA 30'][-1] > df['SMA 100'][-1]:
                 CounterOfChances += 1
             
-            print(Coin, math.floor(19 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)])
+            print(Coin, math.floor(10 / price * MinNotions[Coins.index(Coin)]) / MinNotions[Coins.index(Coin)])
             print('Cycle number - ', i, 'Chances - ', CounterOfChances)
             print(df['RSI'][-1], df['Close'][-1], '\n')
         print('--------------------------')
